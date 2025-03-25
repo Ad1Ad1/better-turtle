98d229aee65ba149eac9e1cafcdc243dbc4029cc
Admin1UserTimGap <148370595+Admin1UserTimGap@users.noreply.github.com>
2025-03-25 23:23:45 +0200

    Create default_presets.py

---
+++ presets/default_presets.py
@@ -0,0 +1,13 @@
+# Default Presets
+# Geometry
+geometry={
+    "triangle":[120,100,3],
+    "square":[90,100,4],
+    "pentagon":[72,100,5],
+    "hexagon":[60,100,6],
+    "circle_1":[1,5,360],
+    "circle_2":[0.2,1,360*5],
+}
+AVAILABLE_FILE_PRESETS={
+    "geometry_shapes":geometry
+}
