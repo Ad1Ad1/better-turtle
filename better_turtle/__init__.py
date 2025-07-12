f0a7386fb341ca0d8fb3cd4577dcd9f7c5e05d82
Ad1 <148370595+Ad1Ad1@users.noreply.github.com>
2025-07-12 14:00:44 +0300

    Python package build - improvement

---
+++ better_turtle/__init__.py
@@ -0,0 +1,5 @@
+from .better_turtle import draw_figure, n_gon
+from .eval_preset import eval_preset, advanced_eval
+
+__version__ = "0.1.0"
+__all__ = ["draw_figure", "n_gon", "eval_preset", "advanced_eval"]
