1334d487c00c5aa486544978c0de9ef5a730070c
Ad1 <148370595+Ad1Ad1@users.noreply.github.com>
2025-07-12 13:41:19 +0300

    Update better_turtle.py

--- better_turtle.py
+++ better_turtle.py
@@ -1,13 +1,13 @@
 from turtle import *
-from eval_preset import eval_preset
+from eval_preset import eval_preset, advanced_eval
 import sys
+import math
 sys.path.append("./presets/")
 from default_presets import AVAILABLE_FILE_PRESETS
 a=AVAILABLE_FILE_PRESETS
 from user_presets import AVAILABLE_FILE_PRESETS
 b=AVAILABLE_FILE_PRESETS
 AVAILABLE_FILE_PRESETS=a|b
-print(AVAILABLE_FILE_PRESETS)
 t=Pen()
 t.up()
 t.goto(0,-300)
@@ -26,4 +26,10 @@ def draw_figure(tm,degree=0,repeat_times=0,PRESET=None,PRESET_SHAPE=None,SPEED=1
                 print("PRESET ERROR: No such name in preset")
         else:
             print("PRESET ERROR: INVALID PRESET. No such available preset specified in current preset file.")
-    done()
+def n_gon(tm,n,scale,SPEED=10):
+    t.speed(SPEED)
+    k=n*math.pi
+    for x in range(0,n):
+        tm.forward(scale*(100/k))
+        tm.left(360/n)
+draw_figure(t,PRESET="test",PRESET_SHAPE="test")
