d5f63cb7f7df7a1ce5abe9af23c9631c16b7e8cc
Ad1 <148370595+Ad1Ad1@users.noreply.github.com>
2025-07-12 14:13:22 +0300

    Python package build - improvement

---
+++ better_turtle/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="better_turtle",
+  version="0.1.0",
+  packages=find_packages(),
+  install_requires=[],
+  author="Ad1Ad1",
+  description="Turtle graphics programming language with variety of features",
+  long_description=open("README.md"),
+  long_description_content_type="text/markdown",
+  url="https://github.com/Ad1Ad1/better_turtle",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
