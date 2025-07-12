52999dfdc142076447c8da0749f907e2476dd20b
Ad1 <148370595+Ad1Ad1@users.noreply.github.com>
2025-07-12 13:45:10 +0300

    Added more default presets

--- presets/default_presets.py
+++ presets/default_presets.py
@@ -1,12 +1,14 @@
 # Default Presets
 # Geometry
 geometry={
-    "triangle":[120,100,3],
-    "square":[90,100,4],
-    "pentagon":[72,100,5],
-    "hexagon":[60,100,6],
-    "circle_1":[1,5,360],
-    "circle_2":[0.2,1,360*5],
+    "triangle":"repeat 3 [ forward 100 left 120 ]",
+    "square":"repeat 4 [ forward 100 left 90 ]",
+    "pentagon":"repeat 5 [ forward 100 left 72 ]",
+    "hexagon":"repeat 6 [ forward 100 left 60 ]",
+    "circle_1":"repeat 360 [ forward 5 left 1 ]",
+    "circle_2":"repeat 1800 [ forward 1 left 0.2 ]",
+    "rectangle":"repeat 2 [ forward 100 left 90 forward 50 left 90 ]",
+    "trapesoid":"forward 100 left 120 forward 50 left 60 forward 80 left 60 forward 50 left 120 forward 30" 
 }
 AVAILABLE_FILE_PRESETS={
     "geometry_shapes":geometry
